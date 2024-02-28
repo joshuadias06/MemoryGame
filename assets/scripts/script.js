28732ae43980ca7eee4bd15b291e69c211de6ee0
Joshua Dias <joshuadiasbf@gmail.com>
2024-02-27 22:08:54 -0300

    Separating View from Logic

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,119 +1,58 @@
-const FRONT ="card_front"
+const FRONT = "card_front"
 const BACK = "card_back"
 const CARD = "card"
 const ICON = "icon"
 
-let techs = ['bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'];
+startGame();
 
-    let cards = null;
+function startGame() {
+    initializeCards(game.createCardsFromTechs());
 
-    startGame();
-
-    function startGame(){
-        cards = createCardsFromTechs(techs);
-        shuffleCards(cards);
-        initializeCards(cards);
-
-    }
-
-    function initializeCards(cards){
-        let gameBoard = document.getElementById("gameBoard")
-
-        cards.forEach(card => {
-
-            let cardElement = document.createElement('div');
-            cardElement.id = card.id;
-            cardElement.classList.add(CARD);
-            cardElement.dataset.icon = card.icon;
-
-            createCardContent(card, cardElement);
-
-            cardElement.addEventListener('click', flipCard);
-            gameBoard.appendChild(cardElement);
-
-        })
-
-    }
-
-    function createCardContent(card, cardElemnt){
-        createCardFace(FRONT, card, cardElemnt);
-        createCardFace(BACK, card, cardElemnt);
-
-    }
-
-    function createCardFace (face, card, element){
-        let cardElementFace = document.createElement('div');
-        cardElementFace.classList.add(face);
-
-        if(face == FRONT){
-
-            let iconElement = document.createElement('img');
-            iconElement.classList.add(ICON)
-            iconElement.src="./assets/images/" + card.icon + ".png"
-            cardElementFace.appendChild(iconElement)
-
-        }else{
-
-            cardElementFace.innerHTML = "&lt/&gt";
-
-        }
-        element.appendChild(cardElementFace);
-    }
-
-    function shuffleCards(cards){
-        let currentIndex= cards.length;
-        let randomIndex = 0;
-
-        while(currentIndex !== 0){
-            randomIndex = Math.floor(Math.random() * currentIndex)
-            currentIndex --;
-
-            [cards[randomIndex], cards [currentIndex]] = [cards[currentIndex],cards[randomIndex]]
-
-        }
-
-
-    }
-
-    function createCardsFromTechs(techs){
-
-        let cards = [];
-
-        techs.forEach((tech)=>{
-
-            cards.push(createPairFromTechs(tech));
-        })
-
-        return cards.flatMap(pair=> pair);
-    }
-
-    function createPairFromTechs(tech){
-
-        return [{
-            id: createIdWithTech(tech),
-            icon: tech,
-            flipped: false,
-        },{
-            id: createIdWithTech(tech),
-            icon: tech,
-            flipped: false,
-
-        }]
-    }
-
-function createIdWithTech(tech){
-    return tech + parseInt(Math.random() * 1000);
 }
 
-function flipCard(){
+function initializeCards() {
+    let gameBoard = document.getElementById("gameBoard")
 
+    game.cards.forEach(card => {
+
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+
+    })
+
+}
+
+function createCardContent(card, cardElemnt) {
+    createCardFace(FRONT, card, cardElemnt);
+    createCardFace(BACK, card, cardElemnt);
+
+}
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if (face == FRONT) {
+
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON)
+        iconElement.src = "./assets/images/" + card.icon + ".png"
+        cardElementFace.appendChild(iconElement)
+
+    } else {
+
+        cardElementFace.innerHTML = "&lt/&gt";
+
+    }
+    element.appendChild(cardElementFace);
+}
+function flipCard() {
+    this.classList.add("flip");
 }
