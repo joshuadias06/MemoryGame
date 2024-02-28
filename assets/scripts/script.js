fadd70fdcb30847f0b1d1082b97f4715d221154a
Joshua Dias <joshuadiasbf@gmail.com>
2024-02-27 21:33:42 -0300

    Create Cards

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,7 @@
 const FRONT ="card_front"
 const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
 
 let techs = [ 'bootstrap',
     'css',
@@ -14,12 +16,84 @@ let techs = [ 'bootstrap',
 
     let cards = null;
 
+    startGame();
+
+    function startGame(){
+        cards = createCardsFromTechs(techs);
+        shuffleCards(cards);
+        initializeCards(cards);
+
+    }
+
+    function initializeCards(cards){
+        let gameBoard = document.getElementById("gameBoard")
+
+        cards.forEach(card => {
+
+            let cardElement = document.createElement('div');
+            cardElement.id = card.id;
+            cardElement.classList.add(CARD);
+            cardElement.dataset.icon = card.icon;
+
+            createCardContent(card, cardElement);
+
+            cardElement.addEventListener('click', flipCard);
+            gameBoard.appendChild(cardElement);
+
+        })
+
+    }
+
+    function createCardContent(card, cardElemnt){
+        createCardFace(FRONT, card, cardElemnt);
+        createCardFace(BACK, card, cardElemnt);
+
+    }
+
+    function createCardFace (face, card, element){
+        let cardElementFace = document.createElement('div');
+        cardElementFace.classList.add(face);
+
+        if(face == FRONT){
+
+            let iconElement = document.createElement('img');
+            iconElement.classList.add(ICON)
+            iconElement.src="./assets/images/" + card.icon + ".png"
+            cardElementFace.appendChild(iconElement)
+
+        }else{
+
+            cardElementFace.innerHTML = "&lt/&gt";
+
+        }
+        element.appendChild(cardElementFace);
+    }
+
+    function shuffleCards(cards){
+        let currentIndex= cards.length;
+        let randomIndex = 0;
+
+        while(currentIndex !== 0){
+            randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex --;
+
+            [cards[randomIndex], cards [currentIndex]] = [cards[currentIndex],cards[randomIndex]]
+
+        }
+
+
+    }
+
     function createCardsFromTechs(techs){
 
-        for(let tech of techs){
+        let cards = [];
 
-            cards.push(createPairFromTechs(tech));;
-        }
+        techs.forEach((tech)=>{
+
+            cards.push(createPairFromTechs(tech));
+        })
+
+        return cards.flatMap(pair=> pair);
     }
 
     function createPairFromTechs(tech){
@@ -38,4 +112,8 @@ let techs = [ 'bootstrap',
 
 function createIdWithTech(tech){
     return tech + parseInt(Math.random() * 1000);
+}
+
+function flipCard(){
+
 }
